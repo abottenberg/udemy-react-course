86e83b250719fc9766f94ebf57b00c06cc7db40e
Alex Bottenberg <alexbottenberg@hotmail.com>
2020-06-22 13:42:47 +0100

    refactoring code and adding loading spinner to app

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
+import LoadingSpinner from './LoadingSpinner'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
 
-    this.state = { lat: null, errorMessage: ''};
+  state = { lat: null, errorMessage: ''};
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude })
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message })
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
@@ -23,10 +20,10 @@ class App extends React.Component {
     }
 
     if (!this.state.err && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return < SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading...</div>;
+    return < LoadingSpinner message="Please accept location request"/>;
   }
 }
 
