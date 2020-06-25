d98b47eb920814a62a7a659900b81fc1a5e0f1c9
Alex Bottenberg <alexbottenberg@hotmail.com>
2020-06-25 14:04:54 +0100

    search bar component

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
 
 class App extends React.Component {
+
+  onTermSubmit = term => {
+    console.log(term)
+  };
+
   render() {
     return (
-      <SearchBar />
+      <div className="ui container">
+        <SearchBar OnFormSubmit={this.onTermSubmit} />
+      </div>
     );
   }
 }
--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onIputChange = (event) => {
+    this.setState({ term: event.target.value })
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    this.props.onTermSubmit(this.state.term);
+  };
+
   render() {
     return (
-      <div>Search bar</div>
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <label>Video search</label>
+          <input
+            type="text"
+            value={this.state.term}
+            onChange={this.onIputChange}
+          />
+        </form>
+      </div>
     );
   }
 }
