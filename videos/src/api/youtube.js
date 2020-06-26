af3313bad27bfc68fba24e54e20f1cfbbd504cc8
Alex Bottenberg <alexbottenberg@hotmail.com>
2020-06-26 13:38:07 +0100

    debugging onFormSubmit

--- videos/src/api/youtube.js
+++ videos/src/api/youtube.js
@@ -1,13 +1,5 @@
-import axios from 'axios';
-
-const KEY = process.env.REACT_APP_API_KEY
+import axios from "axios";
 
 export default axios.create({
-  baseURL: 'https://www.googleapis.com/youtube/v3',
-  params: {
-    require: 'snippet',
-    maxResults: 5,
-    type: 'video',
-    key: KEY
-  }
+  baseURL: "https://www.googleapis.com/youtube/v3"
 });
--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,16 +2,26 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
 
+const KEY = process.env.REACT_APP_API_KEY
+
 class App extends React.Component {
 
   onTermSubmit = term => {
-    console.log(term)
+    youtube.get('/search', {
+      params: {
+        q: term,
+        require: 'snippet',
+        maxResults: 5,
+        type: 'video',
+        key: KEY
+      }
+    });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar OnFormSubmit={this.onTermSubmit} />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
   }
