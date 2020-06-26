91c8e2b515b6565559a9f999bc67383c0745296a
Alex Bottenberg <alexbottenberg@hotmail.com>
2020-06-26 14:40:04 +0100

    adding VideoItem component and css styling

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
 import youtube from '../api/youtube';
+
 
 const KEY = process.env.REACT_APP_API_KEY
 
 class App extends React.Component {
+  state = { videos: [] };
 
-  onTermSubmit = term => {
-    youtube.get('/search', {
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params: {
         q: term,
-        require: 'snippet',
+        part: 'snippet',
         maxResults: 5,
         type: 'video',
         key: KEY
       }
     });
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        < VideoList videos={this.state.videos} />
       </div>
     );
   }
