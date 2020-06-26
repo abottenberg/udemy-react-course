889c19388b34456c74c3824d7941859187cf580d
Alex Bottenberg <alexbottenberg@hotmail.com>
2020-06-26 15:24:17 +0100

    adding VideoDetail component

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 import youtube from '../api/youtube';
 
 
 const KEY = process.env.REACT_APP_API_KEY
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
@@ -22,11 +23,16 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  }
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        < VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        < VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
       </div>
     );
   }
