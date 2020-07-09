2fd83fc21da21ad156ee68e8c8a6f9a190ff6e85
Alex Bottenberg <alexbottenberg@hotmail.com>
2020-07-09 15:36:27 +0100

    adding colour text in selected colour

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -35,16 +35,21 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div ref={ref} className="ui form">
-      <div className="field">
-        <label className="label">Select a colour</label>
-        <div onClick={() => {setOpen(!open)}} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
-          <i className="dropdown icon"></i>
-          <div className="text">{selected.label}</div>
-          <div className={`menu ${open ? 'visible transition' : ''}`}>
-            {renderedOptions}
+    <div>
+      <div ref={ref} className="ui form">
+        <div className="field">
+          <label className="label">Select a colour</label>
+          <div onClick={() => {setOpen(!open)}} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
+            <i className="dropdown icon"></i>
+            <div className="text">{selected.label}</div>
+            <div className={`menu ${open ? 'visible transition' : ''}`}>
+              {renderedOptions}
+            </div>
           </div>
         </div>
+      </div>
+      <div>
+        <h1 style={{color: selected.value}}>{`I love ${selected.label}`}</h1>
       </div>
     </div>
   );
